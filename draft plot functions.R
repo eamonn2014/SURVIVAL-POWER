4062528358635eaeaf0ca5a5c17a892b090cc9ff
eamonn2014 <eamonnjobrien@gmail.com>
2021-06-14 19:48:40 +0100

    adding function

--- draft plot functions.R
+++ draft plot functions.R
@@ -508,17 +508,132 @@ survplot5(  CSurvProp=.5, time1=6, AAR=3 ) #
 
 
 
+set.seed(333)
+simfunf <- function(p=1, hr=2/3, n=352/2, acc=74/4, fup=39/4, lambdaC= -log(.5)/6 , alpha=0.05 ) { # , seed=NULL ) {
+  
+  # p=1
+  # hr=2/3
+  # n=352/2
+  # acc=74/4
+  # fup=39/4
+  # lambdaC= -log(.5)/6
+  # alpha=0.05
+  
+  #
+  #if (!is.null(seed)) set.seed(seed)
+  
+  At <- T1 <- T2 <- D1 <- D2 <- T1a <- T2a <- time1 <- time2 <- l <- NULL
+  
+  At= acc + fup
+  
+  h  <- lambdaC #                    # hazard for exponential using med surv, remeber -log(0.5) is the same 
+  ms <- 1/h*(-log(0.5))^(1)          # lets show we know how to recreate median survival
+  T1 <- 1/h*(-log(runif(n)))^(p)     # weibull times, p=1 so exp, lambda=h
+  
+  ms2<- ms*hr                        # use the HR to get hazard in other group
+  h2 <- -log(0.5)/ms2
+  T2 <- 1/h2*(-log(runif(n)))^(p)    # create n weibull times, p=1 so exp, lambda=h2
+  
+  # lambdaC hazard of censoring
+  C1 = rweibull(n, shape=1, scale=1/lambdaC)   # censoring time
+  C2 = rweibull(n, shape=1, scale=1/lambdaC)   # censoring time
+  
+  a1 <- runif(n,0,acc) # use these for random uniform accrual times
+  a2 <- runif(n,0,acc)
+  
+  D1 = T1+a1   # add rand uniform to weibull events
+  D2 = T2+a2
+  
+  T1a <- ifelse(D1>At, At,  T1 )  # make sure no time + accrual entry exceeds accrual + follow up
+  T2a <- ifelse(D2>At, At,  T2 )  # and replace [rand uniform to weibull events] with original time to event time
+  
+  
+  # by pass censoring, so no censoring!
+  C1 = rep(100000,n)    # censoring time, basically no censoring
+  C2 = rep(100000,n)    # censoring time  basically no censoring
+  
+  time1 = pmin(T1a,C1)  # observed time is min of censored and true grp0
+  time2 = pmin(T2a,C2)  # observed time is min of censored and true grp1
+  
+  event1 = time1==T1a   
+  event2 = time2==T2a   
+  
+  
+  event = c(event1,event2)
+  Ti = c(time1,time2)
+  
+  event <- ifelse(Ti >= At, 0, event)
+  Ti <-     ifelse(Ti >= At, At, Ti)
+  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  dd <- data.frame(T= Ti, 
+                   x = c(rep(1,n), rep(0,n)), 
+                   event = event )
+  
+  l = coxph(Surv(T, event) ~ x, dd)
+  
+  o <- anova(l)$`Pr(>|Chi|)`[2] # P-value from likelihood ratio test
+  
+  pow <- o<=alpha
+  
+  u <- exp(confint(l, level = .9))  [2][[1]]  # use for upper 90% conf interval for non inferiority
+  
+  n <- 100*(l$nevent)/l$n  
+  
+  p <- exp(l$coefficients[[1]])
+  
+  ev <- l$nevent  # no of events
+  
+  L <- summary(l)
+  sd. <- L$coefficients[,"se(coef)"]
+  
+  h1=h
+  h2=h2
+  
+  c(h1,h2,ev,n,p,u,o, sd. , pow)
+}
+
+require(kableExtra)
+simres<- NULL
+set.seed(6987)
+simres <- plyr::raply(9999,simfunf(p=1, hr=3/2, n=352/2, acc=74/4, fup=39/4, lambdaC= -log(.5)/6 , alpha=0.05 ))
+simres <- as.data.frame(simres)
+names(simres) <- c("hazard rate ctrl","hazard rate trt","No of events", "Proportion of events","Mean HR", "HR Upper 90CI","Mean P-value","SD log HR","Power")
+#options(digits=3)
+r <- (apply(simres,2, mean))
+#r<-as.data.frame(r)
+#print(kable(t(r)))
+#print((t(r)))
+#options(digits=7)
 
 
+print(r, digits=6)
 
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
+# This function is useful for creating d1 an input to Hmisc cpower to find the intervention mortality at same time
 
+  morti <- function ( d0=.5, hr=2/3, time=6){
+    
+    lambda = -log(1-d0)/time     # hazard rate 
+    m = 1 - exp(-lambda*time*hr) # mortality hence subtracting from 1
+    return(m)
+  } 
+  
+  
+  d0 <- 0.5      # control mortality at 6 months
+  d1 <- morti()  # interv. mortality at 6 months
+  # r is the % reduction ctrl to intervention
+  
+  cpower(tref=6, n=352, mc=d0, r= 100*(d0 - d1)/d0, tmin=39/4,  #mc is  tref-year mortality, control
+         accrual=74/4, alpha=.05, pr=TRUE,
+         noncomp.c = 0, noncomp.i = 0)
 
-
-
-
-
-
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+  # equivalent survival experiences
+  survplot4(  CSurvProp=.5, time1=6, surv.perc.change.improvement=50 ) 
+  survplot5(  CSurvProp=.5, time1=6, AAR=3 ) # 
+  survplot3( CSurvProp=.5, time1=6, HR=2/3 )  
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 
 
