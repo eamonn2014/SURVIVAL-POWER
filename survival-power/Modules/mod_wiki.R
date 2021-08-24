65d759a6bfd81182b13b81f5dd4668eab7fac25e
eamonn2014 <eamonnjobrien@gmail.com>
2021-08-24 09:36:48 +0100

    adding more text

--- survival-power/Modules/mod_wiki.R
+++ survival-power/Modules/mod_wiki.R
@@ -18,11 +18,22 @@
     ns <- NS(id)
 
     tabItem("wiki",
+            
+     # we can type directly here!        
+     h4(paste("Plugging in alpha, the population variance and the number of replicates (n) into equation [1] we calculate the specification.")),       
+            
+     # or create objects in the server and output here:        
      span(textOutput(ns("warning")), style="color:blue"),
      br(),
 
-     uiOutput(ns("text1"))
+     uiOutput(ns("text1")),
+     
+     uiOutput(ns("text2")),
+     
+     br(),
       )
+    
+    
 
   }
 
@@ -36,18 +47,29 @@
   mod_wiki_server <-  function(input, output, session) {
     ns <- session$ns
 
+      #@@@@@@@@@@@@@@@@ 1
       output$warning <- renderText({
-        paste("The exponential distribution is the simplest parametric distribution used to describe survival distributions. There is a simpler relationship between survival probability, time and hazard with the exponential distribution. We show this relationship")
+       (paste("The exponential distribution is the simplest parametric distribution for describing survival distributions. There is a simpler relationship between survival probability, time and hazard with the exponential distribution. We show this relationship"))
       })
 
+      #@@@@@@@@@@@@@@@@
       output$text1 <- renderUI({
         HTML(paste0(br(),
                     "blah",
                     br(),
                     br(),
                     "blah" ))
-
       })
+        
+      #@@@@@@@@@@@@@@@@ 2
+      output$text2 <- renderUI({ 
+ 
+        withMathJax(
+          helpText(
+            tags$span(style="color:black",
+                      ' $$ {  {\\it{s}^2} =  \\frac{     {(\\sigma^2)}   {(\\chi^2}_{(n - 1), (1-\\alpha)}) }  {(n-1)}          } \\qquad  \\qquad \\qquad  \\qquad \\left[ 1 \\right]    \\!$$')))
+       })  
+      #@@@@@@@@@@@@@@@@ 3
 
 
   }
@@ -69,44 +91,4 @@
   
   
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  # homepage <- function() {
-  #   renderUI({
-  #     tagList(
-  #       fluidRow(
-  #         box(title = h2("Welcome to ReviewR", style='text-align: center;'),
-  #             width = 12,
-  #             status = 'primary', 
-  #             solidHeader = F
-  #         )
-  #       ),
-  #       fluidRow(
-  #         box(width = 12,
-  #             status = 'primary', 
-  #             solidHeader = F, 
-  #             HTML("ReviewR is a portable tool to help you explore data across different data models.  Within ReviewR, you can browse patient data stored in either the OMOP or MIMIC-III data model.<br>"),
-  #             br(),
-  #             HTML("In addition to viewing patient data, you may also connect to a REDCap project to perform a chart review<br>"),
-  #             br(),
-  #             HTML("To get started, please complete the 'Setup' step (found in the left navigation menu)<br>")
-  #         )
-  #       )
-  #     )
-  #   })
-  # }
+   
