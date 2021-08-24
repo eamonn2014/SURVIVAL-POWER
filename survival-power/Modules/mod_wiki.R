bcc0336930bb6ee52d10f8c2f9406964bbc0179b
eamonn2014 <eamonnjobrien@gmail.com>
2021-08-24 22:47:41 +0100

    cosmetic

--- survival-power/Modules/mod_wiki.R
+++ survival-power/Modules/mod_wiki.R
@@ -22,20 +22,20 @@
      # we can type directly here!        
      h4(paste("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.")),       
             
-     div(h4("References:")),  
-     tags$a(href = "https://pubmed.ncbi.nlm.nih.gov/15724232/", tags$span(style="color:blue", h4("[1] Generating survival times to simulate Cox proportional hazards models")),),   
-     div(p(" ")),
-     tags$a(href = "https://github.com/eamonn2014/Survival-power/tree/master/survival-power", tags$span(style="color:blue", h4("[2] Shiny code")),),   
-     div(p(" ")),
-     tags$a(href = "https://stattools.crab.org/R/Survival_Converter.html", tags$span(style="color:blue", h4("[3] Survival converter")),),   
-     div(p(" ")),
-     tags$a(href = "https://stats.stackexchange.com/questions/105881/how-to-simulate-survival-times-using-true-base-line-hazard-function", tags$span(style="color:blue", h4("[4] How to simulate survival times using true base line hazard function")),),   
-     div(p(" ")),
-     tags$a(href = "https://www.youtube.com/watch?v=inMjG32nzcw&ab_channel=Statsols%28ProviderofnQuery%29", tags$span(style="color:blue", h4("[5] Sample Size For Survival Analysis - A guide to planning successful clinical trials")),),   
-     div(p(" ")),
-     tags$a(href = "https://sas-and-r.blogspot.com/2010/03/example-730-simulate-censored-survival.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+SASandR+%28SAS+and+R%29", tags$span(style="color:blue", h4("[6] Simulate censored survival data")),),   
-     div(p(" ")),
-     
+     # div(h4("References:")),  
+     # tags$a(href = "https://pubmed.ncbi.nlm.nih.gov/15724232/", tags$span(style="color:blue", h4("[1] Generating survival times to simulate Cox proportional hazards models")),),   
+     # div(p(" ")),
+     # tags$a(href = "https://github.com/eamonn2014/Survival-power/tree/master/survival-power", tags$span(style="color:blue", h4("[2] Shiny code")),),   
+     # div(p(" ")),
+     # tags$a(href = "https://stattools.crab.org/R/Survival_Converter.html", tags$span(style="color:blue", h4("[3] Survival converter")),),   
+     # div(p(" ")),
+     # tags$a(href = "https://stats.stackexchange.com/questions/105881/how-to-simulate-survival-times-using-true-base-line-hazard-function", tags$span(style="color:blue", h4("[4] How to simulate survival times using true base line hazard function")),),   
+     # div(p(" ")),
+     # tags$a(href = "https://www.youtube.com/watch?v=inMjG32nzcw&ab_channel=Statsols%28ProviderofnQuery%29", tags$span(style="color:blue", h4("[5] Sample Size For Survival Analysis - A guide to planning successful clinical trials")),),   
+     # div(p(" ")),
+     # tags$a(href = "https://sas-and-r.blogspot.com/2010/03/example-730-simulate-censored-survival.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+SASandR+%28SAS+and+R%29", tags$span(style="color:blue", h4("[6] Simulate censored survival data")),),   
+     # div(p(" ")),
+     # 
      
      
      
@@ -51,10 +51,25 @@
 
      uiOutput(ns("text1")),
      
-     uiOutput(ns("text2")),
+    # uiOutput(ns("text2")),
      
      uiOutput(ns("text3")),
      br(),
+     
+     div(h4("References:")),  
+     tags$a(href = "https://pubmed.ncbi.nlm.nih.gov/15724232/", tags$span(style="color:blue", h5("[1] Generating survival times to simulate Cox proportional hazards models - key paper for simulation")),),   
+     div(p(" ")),
+     tags$a(href = "https://github.com/eamonn2014/Survival-power/tree/master/survival-power", tags$span(style="color:blue", h5("[2] Here's the Shiny code")),),   
+     div(p(" ")),
+     tags$a(href = "https://stattools.crab.org/R/Survival_Converter.html", tags$span(style="color:blue", h5("[3] Survival converter")),),   
+     div(p(" ")),
+     tags$a(href = "https://stats.stackexchange.com/questions/105881/how-to-simulate-survival-times-using-true-base-line-hazard-function", tags$span(style="color:blue", h4("[4] How to simulate survival times using true base line hazard function")),),   
+     div(p(" ")),
+     tags$a(href = "https://www.youtube.com/watch?v=inMjG32nzcw&ab_channel=Statsols%28ProviderofnQuery%29", tags$span(style="color:blue", h5("[5] Sample Size For Survival Analysis - A guide to planning successful clinical trials - we duplicate this example")),),   
+     div(p(" ")),
+     tags$a(href = "https://sas-and-r.blogspot.com/2010/03/example-730-simulate-censored-survival.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+SASandR+%28SAS+and+R%29", tags$span(style="color:blue", h5("[6] Simulate censored survival data - useful starting point")),),   
+     div(p(" ")),
+     
       )
     
     
@@ -73,13 +88,15 @@
 
       #@@@@@@@@@@@@@@@@ 1
       output$warning <- renderText({
-       (paste("The exponential distribution is the simplest parametric distribution for describing survival distributions. There is a simpler relationship between survival probability, time and hazard with the exponential distribution. We show this relationship"))
+       (paste("The exponential distribution is the simplest parametric distribution for describing survival distributions. 
+              There is a simple relationship between survival probability, time and hazard with the exponential distribution. Knowing 2 of the 3 parameter completely 
+              determines the remaining parameter."))
       })
 
       #@@@@@@@@@@@@@@@@ 2
       output$text1 <- renderUI({
         HTML(paste0(br(),
-                    "blah",
+                    "where lambda represents hazard rate and S(t) represents survival proportion at time t.",
                     br(),
                     br(),
                     "blah" ))
