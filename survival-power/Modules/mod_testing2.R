2d871cb7437c3a3d7e0a598604190b6ecc83f71c
eamonn2014 <eamonnjobrien@gmail.com>
2021-09-17 10:38:11 +0100

    have histogram in 2 places. 2 submenus

--- survival-power/Modules/mod_testing2.R
+++ survival-power/Modules/mod_testing2.R
@@ -15,13 +15,13 @@
 #' @importFrom shiny NS
 
 
-mod_testing_ui <- function(id){
+mod_testing2_ui <- function(id){
   
   ns <- NS(id)
   
   #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   # the values are created here
-  setupInput<-function(id){
+  setupInput2<-function(id){
     ns<-NS(id)
     tagList(
       sliderInput(ns("bins"), "Number of bins:",
@@ -42,29 +42,29 @@ mod_testing_ui <- function(id){
   
   
   ###~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  tabItem("testing",
+  tabItem("testing2",
           ##~~~~~~~~~~~~~~
           column(width=3,
                  tagList(
                    h4(paste("xxxxxxxxxxxxxxxxxxxxxxxxxxx")),
                    br(),
-                   numericInput(inputId=ns("nsim_input"),                    label = c("No. of simulated studies"),                         value = 30, min=1,  max=1000, step=5),
-                   numericInput(inputId=ns("npergrp_input"),                 label = c("No. of patients per group"),                        value = 100, min=10,  max=1000, step=1),
-                   numericInput(inputId=ns("SurvProp_input"),                label = c("Survival probability"),                            value = 0.5, min=0.01,max=.95,  step=.01),
-                   numericInput(inputId=ns("tSurvNULL_input"),               label = c("Control survival time (green)"),                           value = 4,   min=1,   max=100,  step=1),
-                   numericInput(inputId=ns("tSurvALT_input"),                label = c("Intervention survival time (blue)"),                      value = 7,   min=1,   max=100,  step=1),
+                   # numericInput(inputId=ns("nsim_input"),                    label = c("No. of simulated studies"),                         value = 30, min=1,  max=1000, step=5),
+                   # numericInput(inputId=ns("npergrp_input"),                 label = c("No. of patients per group"),                        value = 100, min=10,  max=1000, step=1),
+                   # numericInput(inputId=ns("SurvProp_input"),                label = c("Survival probability"),                            value = 0.5, min=0.01,max=.95,  step=.01),
+                   # numericInput(inputId=ns("tSurvNULL_input"),               label = c("Control survival time (green)"),                           value = 4,   min=1,   max=100,  step=1),
+                   # numericInput(inputId=ns("tSurvALT_input"),                label = c("Intervention survival time (blue)"),                      value = 7,   min=1,   max=100,  step=1),
                    br(),
                    h4(paste("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.")),
                    br(),
                    
                    #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-                   setupInput("basic"),
+                   #setupInput("basic"),
                    #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
                    
-                   actionButton(ns("resample"),"Hit to run another simulation",icon=icon("bell"), width =300 ,
-                                class = "btn action-button",
-                                style = "color: white;
-                           background-color: blue")
+                   # actionButton(ns("resample"),"Hit to run another simulation",icon=icon("bell"), width =300 ,
+                   #              class = "btn action-button",
+                   #              style = "color: white;
+                   #         background-color: blue")
                  )
                  
                  
@@ -78,12 +78,12 @@ mod_testing_ui <- function(id){
         ),
         
           mainPanel(
-            plotOutput(ns("survplot7")),
+           # plotOutput(ns("survplot7")),
            
             
             #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-            chartUI("first"),     #  new
-           # chartUI("second")     #  new
+            #chartUI("first"),     #  new
+            chartUI("second")     #  new
             #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
             
           )),
@@ -103,7 +103,7 @@ mod_testing_ui <- function(id){
 
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@NEW
 
-  setup<-function(input,output,session){       # hmm
+  setup2<-function(input,output,session){       # hmm
     # How to display all input values in a table
     output$inputs<-renderTable({
       reactiveValuesToList(input)
@@ -111,15 +111,15 @@ mod_testing_ui <- function(id){
      return(input)
   }
    
-  chartUI <- function(id) ({   # this allows multiple plots?
+  chartUI2 <- function(id) ({   # this allows multiple plots?
     ns <- NS(id)
     plotOutput(ns("distPlot"))
   })
   
-  chart <- function(input, output, session, setup) {  # construct plot takes input from 'setup'
+  chart2 <- function(input, output, session, setup2) {  # construct plot takes input from 'setup'
     output$distPlot <- renderPlot({
       x    <- faithful[, 2]
-      bins <- seq(min(x), max(x), length.out = setup$bins + 1)
+      bins <- seq(min(x), max(x), length.out = setup2$bins + 1)
       hist(x,
            breaks = bins,
            col = 'darkgray',
@@ -130,62 +130,62 @@ mod_testing_ui <- function(id){
 #@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@END NEW
 
 
-mod_testing_server <- function(input, output, session){
+mod_testing2_server <- function(input, output, session){
   ns <- session$ns
   
-  plotSettings <- reactiveValues()
-  
-  observeEvent(c(input$resample, 
-                 input$nsim_input, input$npergrp_input, 
-                 input$tSurvNULL_input, input$tSurvALT_input,
-                 input$SurvProp_input  ), {
-                   
-                   plotSettings$A <- input$nsim_input
-                   plotSettings$B <- input$npergrp_input
-                   plotSettings$C <- input$tSurvNULL_input
-                   plotSettings$D <- input$tSurvALT_input
-                   plotSettings$E <- input$SurvProp_input 
-                   
-                 }, ignoreNULL = FALSE)
-  
+  # plotSettings <- reactiveValues()
+  # 
+  # observeEvent(c(input$resample, 
+  #                input$nsim_input, input$npergrp_input, 
+  #                input$tSurvNULL_input, input$tSurvALT_input,
+  #                input$SurvProp_input  ), {
+  #                  
+  #                  plotSettings$A <- input$nsim_input
+  #                  plotSettings$B <- input$npergrp_input
+  #                  plotSettings$C <- input$tSurvNULL_input
+  #                  plotSettings$D <- input$tSurvALT_input
+  #                  plotSettings$E <- input$SurvProp_input 
+  #                  
+  #                }, ignoreNULL = FALSE)
+  # 
   
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
   # https://stackoverflow.com/questions/29496921/how-to-make-shiny-button-re-call-function-for-plot-with-no-input?rq=1
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  observe({   
-    
-    if(input$resample > -1) {   # if 0 plot only appears after first button hit! 
-      
-      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-      survplot7(  nsim     =plotSettings$A, 
-                  npergroup=plotSettings$B,
-                  tSurvNULL=plotSettings$C,
-                  tSurvALT =plotSettings$D,
-                  SurvProp =plotSettings$E) 
-      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-      
-      output$survplot7 <- renderPlot({
-        
-        
-        survplot7(  nsim     =plotSettings$A, 
-                    npergroup=plotSettings$B,
-                    tSurvNULL=plotSettings$C,
-                    tSurvALT =plotSettings$D,
-                    SurvProp =plotSettings$E) 
-        
-      }, height=700, width=1000)
-      #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-      
-    }
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  #new
- 
-   
- 
-    
-    
-    
-  })
-  
+  # observe({   
+  #   
+  #   if(input$resample > -1) {   # if 0 plot only appears after first button hit! 
+  #     
+  #     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  #     survplot7(  nsim     =plotSettings$A, 
+  #                 npergroup=plotSettings$B,
+  #                 tSurvNULL=plotSettings$C,
+  #                 tSurvALT =plotSettings$D,
+  #                 SurvProp =plotSettings$E) 
+  #     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  #     
+  #     output$survplot7 <- renderPlot({
+  #       
+  #       
+  #       survplot7(  nsim     =plotSettings$A, 
+  #                   npergroup=plotSettings$B,
+  #                   tSurvNULL=plotSettings$C,
+  #                   tSurvALT =plotSettings$D,
+  #                   SurvProp =plotSettings$E) 
+  #       
+  #     }, height=700, width=1000)
+  #     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+  #     
+  #   }
+  #   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  #new
+  # 
+  #  
+  # 
+  #   
+  #   
+  #   
+  # })
+  # 
   
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
 }
